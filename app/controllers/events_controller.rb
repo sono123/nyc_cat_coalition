2b515b0b7fe3c0e3e4b057c9bdc19cdfbc02b69c
sono123 <steveono@gmail.com>
2015-08-29 22:58:04 -0400

    Add working event form, including date selectors.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,13 +1,36 @@
 class EventsController < ApplicationController
   def index
+  	@events = Event.all
   end
 
   def new
+  	@event = Event.new
   end
 
   def show
+  	@event = Event.find(params[:id])
+  end
+
+  def create
+  	@event = Event.new(event_params)
+
+  	if @event.save
+      respond_to do |format|
+        format.html { redirect_to @event, notice: 'Event was successfully added.' }
+        format.json { render action: 'show', status: :created, location: @cat }
+      end
+    else
+      format.html { render action: 'new' }
+      format.json { render json: @event.errors, status: :unprocessable_entity }
+    end
   end
 
   def edit
+  end
+
+  private
+
+  def event_params
+  	params.require(:event).permit(:title, :description, :date, :location)
   end
 end
