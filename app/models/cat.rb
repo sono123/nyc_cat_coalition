7bde5780854008c6c31f52e0fd1352f904c9ac23
sono123 <steveono@gmail.com>
2015-09-01 22:02:21 -0400

    Add validations to Cat model.

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -1,3 +1,6 @@
 class Cat < ActiveRecord::Base
 	has_many :pictures, dependent: :destroy
+	validates :name, presence: true, length: { minimum: 2 }
+	validates :description, presence: true, length: { minimum: 10 }
+	validates :age, presence: true
 end
