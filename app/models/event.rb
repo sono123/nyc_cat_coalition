eaa78761c9b57698077dcfe40e4f68dc37c350e2
sono123 <steveono@gmail.com>
2015-09-01 22:14:00 -0400

    Add validations to Event model.

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,6 @@
 class Event < ActiveRecord::Base
+	validates :title, presence: true, length: { minimum: 5 }
+	validates :description, presence: true, length: { minimum: 10 }
+	validates :location, presence: true, length: { minimum: 5 }
+	validates :date, presence: true
 end
