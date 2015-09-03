4f5559574c6dcf32b53be4fc41e64faef28aa28b
sono123 <steveono@gmail.com>
2015-09-02 23:56:45 -0400

    Add validations to contact model.

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,5 @@
 class Contact < ActiveRecord::Base
+	validates :header, presence: true, length: { minimum: 5 }
+	validates :body, presence: true, length: { minimum: 5 }
+	validates :phone, presence: true, length: { minimum: 7 }
 end
