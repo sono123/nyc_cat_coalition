a58491d05519ee4fa6e59f3a51f6a581fc3eb5f3
sono123 <steveono@gmail.com>
2015-09-02 23:55:11 -0400

    Add Contacts#update controller action and strong params.

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -8,5 +8,22 @@ class ContactsController < ApplicationController
   end
 
   def update
+  	@contact = Contact.find(params[:id])
+
+    respond_to do |format|
+      if @contact.update_attributes(contact_params)
+        format.html { redirect_to @contact, notice: 'About was successfully created.' }
+        format.json { render action: 'show', status: :created, location: @contact }
+      else
+        format.html { render action: 'edit' }
+        format.json { render json: @contact.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  def contact_params
+  	params.require(:contact).permit(:header, :body, :phone)
   end
 end
